9b694135b67411eb64b754bcf9e5e3596d723777
BharutJ <bharutj@gmail.com>
2020-05-11 19:38:29 -0400

    Bugfixes

--- Product_Listing/Product_Listing/shoppingCart.Designer.cs
+++ Product_Listing/Product_Listing/shoppingCart.Designer.cs
@@ -38,52 +38,57 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(96, 155);
+            this.dataGridView1.Location = new System.Drawing.Point(65, 70);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(785, 293);
+            this.dataGridView1.Size = new System.Drawing.Size(523, 190);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(251, 89);
+            this.label1.Location = new System.Drawing.Point(168, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(540, 63);
+            this.label1.Size = new System.Drawing.Size(360, 41);
             this.label1.TabIndex = 1;
             this.label1.Text = "ITEMS IN YOUR SHOPPING CART";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(612, 487);
+            this.button1.Location = new System.Drawing.Point(409, 286);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(240, 40);
+            this.button1.Size = new System.Drawing.Size(160, 26);
             this.button1.TabIndex = 2;
             this.button1.Text = "PROCEED TO CHECKOUT";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(110, 487);
+            this.button2.Location = new System.Drawing.Point(74, 286);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(306, 40);
+            this.button2.Size = new System.Drawing.Size(204, 26);
             this.button2.TabIndex = 3;
             this.button2.Text = "EDIT ITEMS IN SHOPPING CART";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // shoppingCart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "shoppingCart";
-            this.Size = new System.Drawing.Size(975, 552);
+            this.Size = new System.Drawing.Size(651, 350);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
